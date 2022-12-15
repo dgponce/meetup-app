2d1146da25c3457df3e1002b4428c9b871896fdb
dgponce <dg38828@gmail.com>
2022-12-15 17:17:15 -0600

    Added meetupForm component

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,54 @@
+import { useRef } from 'react';
+import Card from '../ui/Card';
+import classes from './NewMeetupForm.module.css';
+
+const NewMeetupForm = () => {
+  const titleRef = useRef();
+  const imageRef = useRef();
+  const addressRef = useRef();
+  const descriptionRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredTitle = titleRef.current.value;
+    const enteredImage = imageRef.current.value;
+    const enteredAddress = addressRef.current.value;
+    const enteredDescription = descriptionRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription
+    };
+
+    console.log(meetupData);
+  };
+
+  return <Card>
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor='title'>Meetup Title</label>
+        <input type='text' required id='title' ref={titleRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='image'>Meetup Image</label>
+        <input type='url' required id='image' ref={imageRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='address'>Meetup Address</label>
+        <input type='text' required id='address' ref={addressRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='description'>Meetup Description</label>
+        <textarea id='description' required rows='5' ref={descriptionRef} />
+      </div>
+      <div className={classes.actions}>
+        <button>Add Meetup</button>
+      </div>
+    </form>
+  </Card>
+};
+
+export default NewMeetupForm;
